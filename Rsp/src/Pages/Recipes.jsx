7309b0061be61a9fcf501288cf97446fb0a0028d
Arka <arkapravasantra17@gmail.com>
2025-06-01 23:13:58 +0530

    wadf

--- Rsp/src/Pages/Recipes.jsx
+++ Rsp/src/Pages/Recipes.jsx
@@ -1,17 +1,22 @@
-import React, { useContext } from 'react'
-import { recipeContext } from '../context/RecipeContext'
-import Card from '../Components/Card';
+import React, { useContext } from "react";
+import { recipeContext } from "../context/RecipeContext";
+import Card from "../Components/Card";
 
 const Recipes = () => {
   const { data } = useContext(recipeContext);
 
   const renderRecipe = data.map((recipe) => (
-    <Card key={recipe.id} recipe={recipe}/>
+    <Card key={recipe.id} recipe={recipe} />
   ));
 
   return (
-    <div className='flex flex-wrap gap-2'>
-      {renderRecipe}
+    <div className="min-h-screen bg-gray-50 py-8">
+      <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-bold text-gray-800 mb-8">All Recipes</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          {renderRecipe}
+        </div>
+      </div>
     </div>
   );
 };
